d4abe181cc372cd8045a981b17e1b7329fe49a98
Mikayel Saroyan <mikayelsaroyan@Mikayels-MacBook-Pro.local>
2020-09-08 11:00:29 +0400

    direct mutation fixed

--- app/components/edit-button.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-import {inject as service} from '@ember/service';
-
-
-export default class EditButtonComponent extends Component {
-  @service Global;
-}
